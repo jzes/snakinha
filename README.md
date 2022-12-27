84ed8c53517fe86004a3c6b70feaadd149ec24f9
Jose Paulo <pauloneto.jose@gmail.com>
2022-12-26 21:57:23 -0300

    implement colide? function

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
 # snakinha
 
-FIXME: description
+TODO: description
 
-## Installation
+## TODO Installation
 
-Download from http://example.com/FIXME.
+Download from http://example.com/TODO.
 
-## Usage
+## TODO Usage
 
-FIXME: explanation
+explanation
 
     $ java -jar snakinha-0.1.0-standalone.jar [args]
 
-## Options
+## TODO Options
 
-FIXME: listing of options this app accepts.
+listing of options this app accepts.
 
-## Examples
+## TODO Examples
 
 ...
 
-### Bugs
+### TODO Bugs
 
 ...
 
@@ -28,17 +28,3 @@ FIXME: listing of options this app accepts.
 ### That You Think
 ### Might be Useful
 
-## License
-
-Copyright © 2022 FIXME
-
-This program and the accompanying materials are made available under the
-terms of the Eclipse Public License 2.0 which is available at
-http://www.eclipse.org/legal/epl-2.0.
-
-This Source Code may also be made available under the following Secondary
-Licenses when the conditions for such availability set forth in the Eclipse
-Public License, v. 2.0 are satisfied: GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or (at your
-option) any later version, with the GNU Classpath Exception which is available
-at https://www.gnu.org/software/classpath/license.html.
--- src/snakinha/core.clj
+++ src/snakinha/core.clj
@@ -1,6 +1,8 @@
 (ns snakinha.core
   (:gen-class))
 
+;; TODO separar em um logic 
+;; TODO criar testes
 (defn clear-screen []
   (Thread/sleep 70)
   (print (str (char 27) "[2J"))
@@ -14,8 +16,8 @@
        (repeat y)
        (into [])))
 
-(defn bear-fruit
-  ([x y board]
+(defn bear-fruit ;; TODO mudar pro mesmo esquema da snake (lista de coordenadas) 
+  ([x y board]    ;; e não fazer direto no board, fica mais facil checar
    (bear-fruit x y board "f"))
   ([x y board fruit]
    (update-in board [x y] (fn [_] fruit))))
@@ -24,7 +26,7 @@
   ([snake board] (draw-snake snake board "s"))
   ([snake board snake-sprite]
    (reduce
-    (fn [board snake-point]
+    (fn [board snake-point] ;; TODO fazer um destructuring aqui
       (update-in board
                  [(:x snake-point) (:y snake-point)]
                  (fn [_] (if (:head snake-point)
@@ -33,16 +35,28 @@
     board
     snake)))
 
+(defn colide?
+  [snake
+   {:keys [x y]}]
+  (boolean (some (fn [p] (and (= (:x p) x) (= (:y p) y))) snake)))
+
 (defn move-snake
-  [direction snake]
+  [coordinate-update-fn coordinate-update-axis snake]
   (let [head (first (filter :head snake))
-        body (into [] (map #(update % :head (fn [_] false)) (rest snake)))]
-    (cond
-      (= direction "right") (conj body (update head :y inc))
-      (= direction "down") (conj body (update head :x inc))
-      (= direction "up") (conj body (update head :x (fn [x] (if (= x 0)
-                                                              0
-                                                              (+ x 1))))))))
+        body (into [] (map #(update % :head (fn [_] false)) (rest snake))) ;; TODO transformar em 2 passos
+        new-head (assoc head coordinate-update-axis (coordinate-update-fn (coordinate-update-axis head))) ;; TODO transformas em threading last
+        colide (colide? snake new-head)]
+    (if (or (= (coordinate-update-axis new-head) 0) colide)
+      snake
+      (conj body new-head))))
+
+(defn move-snake-to ;TODO transformar as directions em keywords e checar se existe um enum em clojure
+  [direction snake]
+  (cond
+    (= direction "right") (move-snake inc :y snake)
+    (= direction "down") (move-snake inc :x snake)
+    (= direction "up") (move-snake dec :x snake)
+    (= direction "left") (move-snake dec :y snake)))
 
 (defn render-board
   [board]
@@ -55,28 +69,37 @@
         body (into [] (butlast snake))]
     (conj body (assoc head :head true))))
 
-(defn colide?
-  [snake
-   {:keys [x y]}]
-  (boolean (some (fn [p] (= p {:x x :y y})) snake)))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [board (make-board 6 6 "x")
         fruited-board (bear-fruit 1 1 board)
-        snake (atom (raise-snake 3))]
+        snake (atom (raise-snake 4))]
+    (println "---------start------------")
     (println (render-board (draw-snake @snake board)))
-    (swap! snake (partial move-snake "down"))
-    (println "--------------------------")
+    (swap! snake (partial move-snake-to "down"))
+    (println "---------down-------------")
     (println (render-board (draw-snake @snake board)))
-    (swap! snake (partial move-snake "down"))
-    (println "--------------------------")
+    (swap! snake (partial move-snake-to "down"))
+    (println "---------down-------------")
     (println (render-board (draw-snake @snake board)))
-    (swap! snake (partial move-snake "down"))
-    (println "--------------------------")
+    (swap! snake (partial move-snake-to "down"))
+    (println "---------down-------------")
     (println (render-board (draw-snake @snake board)))
-    (swap! snake (partial move-snake "right"))
-    (println "--------------------------")
-    (println (render-board (draw-snake @snake board))) 
-    ))
+    (swap! snake (partial move-snake-to "up"))
+    (println "---------up--colide---------")
+    (println (render-board (draw-snake @snake board)))
+    (swap! snake (partial move-snake-to "right"))
+    (println "---------right---------------")
+    (println (render-board (draw-snake @snake board)))
+    (swap! snake (partial move-snake-to "up"))
+    (println "---------up---------------")
+    (println (render-board (draw-snake @snake board)))
+    (swap! snake (partial move-snake-to "down"))
+    (println "---------down--colide---------------")
+    (println (render-board (draw-snake @snake board)))
+    (swap! snake (partial move-snake-to "left"))
+    (println "---------left---------------")
+    (println (render-board (draw-snake @snake board)))))
