673f4ec1f2b252433109714c1b268bc2679a4597
Jose Paulo <pauloneto.jose@gmail.com>
2023-01-02 16:47:32 -0300

    add some TODOs to fruits

--- src/snakinha/logic/fruit.clj
+++ src/snakinha/logic/fruit.clj
@@ -8,15 +8,15 @@
         new-fruit {:x x :y y}]
     (when (or (> x board-x-len) (> y board-y-len))
       (throw (Exception. "fruit out of bounds")))
-    (when (snake/colide? snake new-fruit)
-      (throw (Exception. "fruit cant bear over snake")))
+    (when (snake/colide? snake new-fruit) ;; TODO <-------- talvez o ideal seja isso não estar
+      (throw (Exception. "fruit cant bear over snake"))) ;;  aqui, ou recebermos como dep injetada
     new-fruit))
 
 (defn bear-randon-fruit
   [board snake]
   (let [board-x-len (count board)
         board-y-len (count (get board 0))
-        randon-x (rand-int board-x-len)
+        randon-x (rand-int board-x-len) ;; TODO receber func de random injetada
         randon-y (rand-int board-y-len)]
     (bear-fruit randon-x randon-y board snake)))
 
